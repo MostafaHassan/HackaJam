15679b9d2e8993f01c2c40b665d57bbe936e4ae9
MostafaHassan <mostafahassan93@hotmail.com>
2015-06-14 01:21:51 +0200

    test model

---
+++ HackaJam/Assets/Scenes/Tail.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tail : MonoBehaviour {
+
+	public GameObject player1;
+	public GameObject player2;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		Vector2 vec2 = new Vector2 (player1.transform.position.x, player1.transform.position.y);
+
+	}
+}
