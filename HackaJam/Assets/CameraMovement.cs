bb0f7bda08916154855eae9458beab2a6485a706
Jawsarn <christian_markowicz@hotmail.com>
2015-06-14 00:25:35 +0200

    Fixed camera

--- HackaJam/Assets/CameraMovement.cs
+++ HackaJam/Assets/CameraMovement.cs
@@ -5,7 +5,9 @@ public class CameraMovement : MonoBehaviour {
 
 	public GameObject player1;
 	public GameObject player2;
-	Camera cam;
+	float maxDistance = 10f;
+	float speed = 0.1f;
+
 
 	// Use this for initialization
 	void Start () {
@@ -16,5 +18,18 @@ public class CameraMovement : MonoBehaviour {
 	void Update () {
 		Vector3 myFocus = player1.transform.position + (player2.transform.position - player1.transform.position)/2.0f;
 		transform.LookAt (myFocus);
+
+		float playerZ = Mathf.Min (player1.transform.position.z, player2.transform.position.z);
+
+		float cameraZ = transform.position.z;
+
+
+
+		if (playerZ - cameraZ > maxDistance) 
+		{
+			transform.position = transform.position + new Vector3(myFocus.x, 0, myFocus.z).normalized*(playerZ - cameraZ)*Time.deltaTime*speed;
+		}
+
+		
 	}
 }
